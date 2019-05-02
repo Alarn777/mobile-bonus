d6210ed555d9ac55de64f58fba949bc0f7930eb5
michael rokitko <alarn777@gmail.com>
2019-05-02 12:29:26 +0300

    navigation working

---
+++ screens/Gyroscope.js
@@ -0,0 +1,14 @@
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from "react-native";
+
+
+export default class Gyroscope extends React.Component {
+    render() {
+        return (
+            <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+                <Text>Gyroscope Screen</Text>
+            </View>
+        );
+    }
+}
